c868d8ab0255acdef5f8184dac43ba1eac8b7a7a
Corbin <kalsonofel03@gmail.com>
2026-04-17 10:50:13 -0500

    updated with sign in and vinyl image

--- SWpj/testApp/views.py
+++ SWpj/testApp/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request,"home.html")
     
 def about(request):
     return render(request,"about.html")
+    
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()              # creates the user
+            return redirect('/accounts/login/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
+
